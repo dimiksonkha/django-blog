e6a64bcb084f784071ba0b8f2178683396deab7e
xHacker404 <sulaiman.haque@gmail.com>
2019-01-04 08:52:45 +0600

    Static Files Added

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+
+    //Social Share 
+    $("#share").jsSocials({
+            shares: ["email", "twitter", "facebook", "googleplus", "linkedin", "pinterest", "stumbleupon", "whatsapp"]
+    });    
+        
+    //Form Validation
+    $("#signup").validate();
+    
+}); 
