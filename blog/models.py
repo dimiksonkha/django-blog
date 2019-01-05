a640fc9686219788f9d3a580517a673fbc319555
xHacker404 <sulaiman.haque@gmail.com>
2019-01-05 11:00:06 +0600

    All Works Related Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250,unique=True)
+    content = models.CharField(max_length=1000)
+    author = models.CharField(max_length=100, default="xHacker404")
+    published_date = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.title
+
+
+
+class Comment(models.Model):
+    #post = models.ForeignKey(Post)  //have to fix this
+    title = models.CharField(max_length=250,unique=True)
+    content = models.CharField(max_length=1000)
+    author = models.CharField(max_length=100, default="xHacker404")
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post, Comment
 # Create your views here.
 
 def index(request):
-    my_dict = {'msg':"Blog Engine Developed By xHacker404"}
+    post_list = Post.objects.order_by('published_date')
+    my_dict = {'posts':post_list}
     return render(request, 'blog/index.html', context=my_dict)
