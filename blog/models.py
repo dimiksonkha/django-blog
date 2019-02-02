8b588e70b9a0ab4b091500365c21a2ad30cb2c63
xHacker404 <sulaiman.haque@gmail.com>
2019-02-02 11:13:53 +0600

    models updated.

--- blog/models.py
+++ blog/models.py
@@ -22,8 +22,18 @@ class Post(models.Model):
     category = models.CharField(max_length=150, default='uncategoried')
     featured_img = models.ImageField(upload_to='featured_images', blank=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE, default=1)
-    published_date = models.DateTimeField(default=datetime.now())
+    created_date = models.DateTimeField(default=datetime.now())
+    published_date = models.DateTimeField(blank=True, null=True)
 
+    def publish(self):
+        self.published_date = datetime.now()
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(is_approved = True)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_details', args=(post_id, ))
 
     def __str__(self):
         return self.title
@@ -31,15 +41,25 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE, default=1, related_name='comments')  #have to fix this
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, default=1, related_name='comments')
     title = models.CharField(max_length=250,blank=True)
     content = models.CharField(max_length=1000, blank=True)
     author = models.ForeignKey(UserProfileInfo,on_delete=models.CASCADE,default=1)
-    published_date = models.DateTimeField(default=datetime.now(), blank=True)
+    created_date = models.DateTimeField(default=datetime.now())
+    published_date = models.DateTimeField(null=True, blank=True)
+    is_approved = models.BooleanField(default=False)
 
+    def approve(self):
+        self.is_approved = true
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_details', args=(post_id, ))
 
     def __str__(self):
         return self.title
+
+
 
 class Reply(models.Model):
     comment = models.ForeignKey(Comment,on_delete=models.CASCADE, default=10, related_name='replies')  #have to fix this
@@ -47,6 +67,6 @@ class Reply(models.Model):
     content = models.CharField(max_length=1000, blank=True)
     author = models.ForeignKey(UserProfileInfo,on_delete=models.CASCADE,default=1)
     published_date = models.DateTimeField(default=datetime.now(), blank=True)
-    
+
     def __str__(self):
         return self.title
