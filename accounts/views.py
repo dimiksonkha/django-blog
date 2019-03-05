d2d7a5c0e386193418bfd26e0c3ab7d3b9217cb8
AZGHOUR-SAAD <saad.azghour@gmail.com>
2019-03-05 13:00:36 +0100

    Custom Error Page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from blog.models import Post
+from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from accounts.forms import UserForm,UserProfileInfoForm
 
 # Create your views here.
+
+#Index page with latest posts
+def index(request):
+    post_list = Post.objects.order_by('published_date')
+
+    paginator = Paginator(post_list, 5) # Show 5 posts per page
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    my_dict = {'posts':posts}
+    return render(request, 'blog/index.html', context=my_dict)
+
+#Log out from blog
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+#Authentication and Login
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active and ( user.is_superuser or user.is_staff):
+                login(request, user)
+                return HttpResponseRedirect(reverse('backend:posts'))
+            elif user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("User is not active.Please Contact  to Admin")
+
+        else:
+            print('Someone tried to login and failed!')
+            print('User Name {} and Password {}'.format(username, password))
+            return HttpResponse("Invalid login detials provided!!!")
+    else:
+        return render(request,'accounts/login.html', {})
+
+
+#New user registration
+def registration(request):
+    registered = False
+
+    if request.method == 'POST' :
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit = False)
+            profile.user = user
+
+            if 'profile_pic' in request.FILES :
+                profile.profile_pic = request.FILES['profile_pic']
+
+            profile.save()
+
+            registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request,'accounts/sign-up.html',
+                    {'user_form':user_form,
+                    'profile_form':profile_form,
+                    'registered':registered})
--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from blog.models import Post,Tag,Category
 from accounts.models import UserProfileInfo
 from comments.models import Comment,Reply
 from .models import BlogSettings
-from blog.forms import UserForm,UserProfileInfoForm
+from accounts.forms import UserForm,UserProfileInfoForm
 from django.core.paginator import Paginator
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
@@ -125,13 +125,20 @@ def create_post(request):
 #Post edit page
 @login_required
 def edit_post(request, pk):
-    post = Post.objects.get(id=pk)
+    post = get_object_or_404(Post, id=pk)
     post_tags = post.tag.all()
     other_tags = Tag.objects.all().exclude(tags__pk=pk)
     post_categories = post.category.all()
     other_categories = Category.objects.all().exclude(categories__pk=pk)
 
-    my_dict = {'post':post, 'edit_post':'edit_post', 'post_tags':post_tags, 'post_categories':post_categories,'other_tags':other_tags,'other_categories':other_categories}
+    my_dict = {
+        'post':post,
+        'edit_post':'edit_post',
+        'post_tags':post_tags,
+        'post_categories':post_categories,
+        'other_tags':other_tags,
+        'other_categories':other_categories
+    }
 
     return render(request, 'backend/posts.html', context=my_dict)
 
@@ -184,7 +191,7 @@ def update_post(request):
 # process delete post
 @login_required
 def delete_post(request, pk):
-    post = Post.objects.get(id=pk)
+    post = get_object_or_404(Post, id=pk)
     post.delete()
 
     if(request.user.is_staff):
