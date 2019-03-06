b4e6460172467a34a8a0efd9bbc565f26c12c560
xHacker404 <sulaiman.haque@gmail.com>
2019-03-06 06:44:19 +0600

    Reverting

--- blog_engine/urls.py
+++ blog_engine/urls.py
@@ -38,7 +38,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('backend/', include('backend.urls')),
     path('accounts/', include('accounts.urls')),
-    path('comments/', include('comments.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
