c9b8f0858a426cb3d9924084846a90056a6d4214
dimiksonkha <sualiman.haque@gmail.com>
2021-12-19 14:31:18 +0600

    requirements.txt updated

--- blog/views.py
+++ blog/views.py
@@ -68,11 +68,11 @@ def custom_500(request):
     return render(request, '500.html', status=500)
 
 
-def custom_400(request):
+def custom_400(request, exception):
     return render(request, '400.html', status=400)
 
 
-def custom_403(request):
+def custom_403(request,exception):
     return render(request, '403.html', status=403)
 
 
---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ayaan/Projects/django-blog/djangoenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
